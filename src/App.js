8205d646f14441ddbd0837e3b9f6fa670f85725e
Omar_Perez <omar1987perez2@gmail.com>
2022-12-13 19:03:27 +0100

    elimino header organizo componentes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
-import './App.css';
-import Header from './components/Header/Header';
-import CardVideo from './components/CardVideo/CardVideo';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import CardVideo from "./components/CardVideo/CardVideo";
 
 function App() {
+  const [stateNumber, setStateNumber] = useState(0);
+  const [videoApi, setVideoApi] = useState();
+  const [titleApi, setTitleApi] = useState();
+
+  const resPorPagina = 10;
+  const key = process.env.REACT_APP_API_KEY2;
+  const idCanal = process.env.REACT_APP_ID_CANAL;
+
+  const url = `https://www.googleapis.com/youtube/v3/search?key=${key}&channelId=${idCanal}&part=snippet,id&order=date&maxResults=${resPorPagina}`;
+
+  useEffect(() => {
+    const FetchApi = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setVideoApi(data.items[stateNumber].id.videoId);
+      setTitleApi(data.items[stateNumber].snippet.title);
+    };
+    FetchApi();
+  }, [stateNumber]);
+
+
+
+  const disminuir = () => {
+    setStateNumber(stateNumber <= 0 ? 9 : stateNumber - 1);
+  };
+
+  const aumentar = () => {
+    setStateNumber(stateNumber >= 9 ? 0 : stateNumber + 1);
+  };
 
   return (
-    <div className='general'>
-      <div className='container'>
-        <Header />
-      </div>
-      <div>
-        <CardVideo />
-      </div>
-    </div>
+    <>
+      <section>
+        <CardVideo
+          videoApi={videoApi}
+          titleApi={titleApi}
+          disminuir={disminuir}
+          aumentar={aumentar}
+        />
+      </section>
+    </>
   );
 }
 
--- src/components/CardVideo/CardVideo.js
+++ src/components/CardVideo/CardVideo.js
@@ -1,85 +1,43 @@
+import { useState } from "react";
+import ListLike from "../ListLike/ListLike";
+import "./CardVideo.css";
 
-import React, {useEffect, useState} from 'react';
-import ListLike from '../ListLike/ListLike';
-import './CardVideo.css'
+function CardVideo({ titleApi, videoApi, disminuir, aumentar}) {
 
-
-function CardVideo () {
-    
-    const [stateNumber, setStateNumber] = useState(0)
-    
-    const [videoApi, setVideoApi] = useState()
-    const [titleApi, setTitleApi] = useState()
-    const [clic, setClic] = useState(false)
-
-    
-    
-    const resPorPagina = 10;
-    const key = process.env.REACT_APP_API_KEY2;
-    const idCanal = process.env.REACT_APP_ID_CANAL;
-    
-    
-    const url = `https://www.googleapis.com/youtube/v3/search?key=${key}&channelId=${idCanal}&part=snippet,id&order=date&maxResults=${resPorPagina}`
-    
-    
-    const disminuir = () => {
-        setStateNumber(stateNumber <= 0 ? 9 : stateNumber -1)
-    }
-
-    const aumentar = () => {
-        setStateNumber(stateNumber >= 9 ? 0 : stateNumber +1)
-    }
+    const [clic, setClic] = useState(false);
 
     function eventoClick() {
         setClic(titleApi);
       }
-    
-    
-    useEffect(() => {
-        const FetchApi = async () => {
-            const response = await fetch(url);
-            const data = await response.json();
 
-            setStateNumber(stateNumber)
-            
-            setVideoApi(data.items[stateNumber].id.videoId)
-            setTitleApi(data.items[stateNumber].snippet.title)
-                    
-            };
-                FetchApi();
-            }, [stateNumber]);
-            
-    return (
-        <div>
-
-            <div className='aside'>
-                <div className="container_card">
-                    <div className="video">
-
-                        <iframe className='reproductor' src={`https://www.youtube.com/embed/${videoApi}?modestbranding=1&rel=0&showinfo=0&autoplay=1`}
-                        title={titleApi} allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+  return (
+    <section>
+      <div className="aside">
+        <div className="container_card">
+          <div className="video">
+            <iframe
+              className="reproductor"
+              src={`https://www.youtube.com/embed/${videoApi}?modestbranding=1&rel=0&showinfo=0&autoplay=1`}
+              title={titleApi}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
                         gyroscope; picture-in-picture"
-                        ></iframe> 
-                        
-                    </div>
+            ></iframe>
+          </div>
 
-                    <div className='position-footer'>
-                        <button onClick={disminuir} className="reverse">◀</button>
-                        <button onClick={aumentar} className="forward">▶</button>
-                        <button onClick={eventoClick} className="like">❤</button>
-                    </div>
-                    <div className='containerTitle'>
-                        <h4 className="title" key={titleApi} >🎬... {titleApi}</h4>
-                    </div>
-
-                </div>
-            </div>
-                    <div>
-                        <ListLike titulo={titleApi} clic={clic} />
-                    </div>
+          <div className="position-footer">
+            <button onClick={disminuir} className="reverse">◀</button>
+            <button onClick={aumentar} className="forward">▶</button>
+            <button onClick={eventoClick} className="like"> ❤</button>
+          </div>
+          <div className="containerTitle">
+            <h4 className="title">🎬... {titleApi}
+            </h4>
+          </div>
         </div>
-    )
-
+      </div>
+      <ListLike clic={clic}/>
+    </section>
+  );
 }
 
-export default CardVideo
+export default CardVideo;
